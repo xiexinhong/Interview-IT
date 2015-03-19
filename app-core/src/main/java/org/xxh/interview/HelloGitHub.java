0ea1f5089cbf0f7101dba06f2d68da70aafa1f98
谢欣宏 <xinhong.xie@renren-inc.com>
2015-03-19 16:22:02 +0800

    change commit

--- app-core/src/main/java/org/xxh/interview/HelloGitHub.java
+++ app-core/src/main/java/org/xxh/interview/HelloGitHub.java
@@ -1,7 +1,15 @@
 package org.xxh.interview;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Administrator on 2015/3/19.
  */
-public class HelloGitHub {
+public class HelloGitHub extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
